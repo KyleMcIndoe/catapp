7eb991a981c9b2819d7aeadea0395941a039764d
KyleMcIndoe <kmcindoe06@gmail.com>
2024-10-13 16:18:27 -0400

    Get random cat fact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,23 +6,36 @@ import axios from 'axios'
 
 function App() {
 
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
 
   useEffect(() => {
+    getFact()
+  }, [])
+
+  function getFact() {
     axios.get('http://127.0.0.1:8000/api/facts/')
     .then(res => {
       console.log(res.data)
-      setTitle(res.data[0].title)
-      setBody(res.data[0].body)
+      let obj = Object.keys(res.data)
+      let i = getRandomInt(obj.length)
+      setTitle(res.data[i].title)
+      setBody(res.data[i].body)
     })
     .catch(err => console.log(err))
-  }, [])
+  }
 
   return (
     <>
-      {title}
-      {body}
+      <h1 className='title'>{title}</h1>
+      <p className='body'>{body}</p>
+      <button onClick={() => {
+        getFact()
+      }}>Get cat fact</button>
     </>
   )
 }
